62c8df9ed842958e76e8eaaaf7e8ab477b12e776
Dylan Buchi <devybuchi@gmail.com>
2022-11-10 12:51:34 -0300

    Add error handler middleware and productsRouter

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import express, { Request, Response } from "express";
 
 import { settings } from "./config/settings";
 import { connectToDatabase } from "./databases/connect";
+import { errorHandlerMiddleware } from "./middleware/error-handler";
 import { notFoundMiddleware } from "./middleware/not-found";
+import { productsRouter } from "./routes/products";
 
 class App {
   constructor(private app = express()) {
@@ -11,6 +13,8 @@ class App {
       res.send("API is running");
     });
 
+    app.use("/api/v1/products", productsRouter);
+    app.use(errorHandlerMiddleware);
     app.use(notFoundMiddleware);
   }
 
