11dd03aec4723226c7a563cb166b1ed20cda5a07
Dylan Buchi <devybuchi@gmail.com>
2022-11-11 08:34:11 -0300

    Add products data

---
+++ src/databases/products.ts
@@ -0,0 +1,133 @@
+import { IProduct } from "../models/interfaces/product";
+
+export const products: IProduct[] = [
+  {
+    name: "accent chair",
+    price: 25,
+    company: "amara",
+    rating: 4,
+  },
+  {
+    name: "albany sectional",
+    price: 109,
+    company: "horne",
+    rating: 5,
+  },
+  {
+    name: "albany table",
+    price: 309,
+    company: "horne",
+    rating: 4.9,
+  },
+  {
+    name: "armchair",
+    price: 125,
+    company: "amara",
+    rating: 4.8,
+  },
+  {
+    name: "bar stool",
+    price: 40,
+    company: "horne",
+    rating: 4.6,
+  },
+  {
+    name: "dining table",
+    price: 42,
+    company: "ikea",
+    rating: 4.55,
+  },
+  {
+    name: "emperor bed",
+    price: 23,
+    company: "ikea",
+  },
+  {
+    name: "entertainment center",
+    price: 59,
+    featured: true,
+    company: "terrain",
+  },
+  {
+    name: "high-back bench",
+    price: 39,
+    featured: true,
+    company: "ikea",
+  },
+
+  {
+    name: "leather sofa",
+    price: 99,
+    company: "terrain",
+  },
+  {
+    name: "modern bookshelf",
+    price: 31,
+    featured: true,
+    company: "terrain",
+  },
+  {
+    name: "modern poster",
+    price: 30,
+    company: "horne",
+  },
+  {
+    name: "shelf",
+    price: 30,
+    company: "ikea",
+  },
+  {
+    name: "simple chair",
+    price: 109,
+    company: "horne",
+  },
+  {
+    name: "sofa set",
+    price: 129,
+    company: "amara",
+  },
+  {
+    name: "suede armchair",
+    price: 15,
+    company: "terrain",
+  },
+  {
+    name: "utopia sofa",
+    price: 79,
+    featured: true,
+    company: "horne",
+  },
+  {
+    name: "vase table",
+    price: 120,
+    featured: true,
+    company: "amara",
+  },
+  {
+    name: "wooden bed",
+    price: 25,
+    company: "ikea",
+  },
+  {
+    name: "wooden desk",
+    price: 15,
+    company: "ikea",
+  },
+  {
+    name: "wooden desk",
+    price: 40,
+    company: "ikea",
+  },
+  {
+    name: "wooden table",
+    price: 23,
+    featured: true,
+    company: "terrain",
+  },
+  {
+    name: "a first wooden table",
+    price: 23,
+    featured: true,
+    company: "terrain",
+  },
+];
